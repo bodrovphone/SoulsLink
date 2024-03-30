bb50bf3e2e6c408c0342dd450405cbd7232a1531
oleksandr.bodrov <ext-oleksandr.bodrov@almamedia.com>
2024-03-30 20:38:10 +0200

    Header with Icon completed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import Title from './components/Title/Title';
+
+import globalStyle from './assets/styles/globalStyle';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Title title="Hello World" />
+      <View style={globalStyle.header}>
+        <Title title="Hello World" />
+        <TouchableOpacity style={globalStyle.messageIcon}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumberText}>2</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
